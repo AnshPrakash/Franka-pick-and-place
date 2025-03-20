83b96545d77b402dd5131c9a177455c7e06fe98f
Ansh <anshprakash@gmail.com>
2025-03-20 18:40:21 +0100

    init controller

--- src/control/ik_solver.py
+++ src/control/ik_solver.py
@@ -1,31 +1,58 @@
-from typing import Dict, Any, Optional
-
 import numpy as np
-import pybullet as p
-
-# from src.robot import Robot
-# from src.simulation import Simulation
-# from src.utils import pb_image_to_numpy
-
+from src.simulation import Simulation
 import robotic as ry
-import numpy as np
-import time
-
-C = ry.Config()
-C.addFile(ry.raiPath('scenarios/pandaSingle.g'))
-C.view(True)
-
-C.addFrame('box') \
-    .setPosition([-.25,.1,1.]) \
-    .setShape(ry.ST.ssBox, size=[.06,.06,.06,.005]) \
-    .setColor([1,.5,0]) \
-    .setContact(1)
-C.view(True)
-
 
 class IKSolver:
-    """IKSolver Class.
+    def __init__(self, sim: Simulation):
+        """
+            Initialize KOMO planner with robot configuration.
+            Estimate the robot's joint configuration given the target end-effector position 
+            and orientation.
+        """
+        self.sim = sim
 
-    The class initializes
-    """
-    pass
+        C = ry.Config()
+        C.addFile(ry.raiPath('scenarios/pandaSingle.g'))
+        self.C = C
+        
+
+
+    def compute_target_configuration(self, target_pos, target_ori):
+        """
+        Compute the robot's joint configuration given the target end-effector position and orientation.
+        """
+
+        # Get current robot state
+        joint_states = self.sim.robot.get_joint_positions()
+
+        # Update KOMO with new state
+        self.C.setJointState(joint_states)
+
+        # Initialize KOMO solver
+        komo = ry.KOMO()
+        komo.setConfig(self.C, True)
+
+
+        # Get joint limits
+        j_lower, j_upper = self.sim.robot.get_joint_limits()
+
+        # Get Wall and base positions
+        base_pos = self.sim.get_base_position()
+        table_pos = self.sim.get_table_position()
+
+        # Get current state
+        joint_states = self.sim.robot.get_joint_positions()
+
+    
+        # Define optimization problem
+        komo.set_end_effector_target(target_pos, target_ori)
+
+        # Solve for new joint positions & Target position
+        ret = ry.NLP_Solver(komo.nlp(), verbose=0 ) .solve()
+        if ret.feasible:
+            print('-- Always check feasibility flag of NLP solver return')
+        else:
+            print('-- THIS IS INFEASIBLE!')
+
+        q = komo.getPath()
+
